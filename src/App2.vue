fd4d7825965cbdb14369fa76a0556fe8c587885d
judychang <judy8210276834@gmail.com>
2023-06-10 18:19:26 +0800

    註冊頁面串接api

---
+++ src/App2.vue
@@ -0,0 +1,16 @@
+<script>
+import Test from "./components/Test.vue";
+export default {
+    components: {
+        Test,
+    }
+}
+</script>
+
+<template>
+  <Test />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,133 @@
+
+<script>
+import { reactive } from "vue";
+import axios from "axios"
+export default {
+  setup() {
+    const registered = reactive({
+      name: "",
+      email: "",
+      password: "",
+    });
+
+    const handSubmit = () => {
+      axios
+        .post("http://localhost:8000/register", {
+         name:registered.name,
+         email:registered.email,
+         password:registered.password,
+        })
+        .then( (response) => {
+          console.log(response);
+        })
+        .catch( (error) => {
+          console.log(error);
+        });
+    };
+    return {
+      registered,
+      handSubmit,
+    };
+  },
+};
+</script>
+
+<template>
+  <section>
+    <div class="form-container">
+      <h1>login form</h1>
+      <form>
+        <div class="control">
+          <input
+            type="text"
+            placeholder="請輸入姓名"
+            v-model="registered.name"
+          />
+        </div>
+        <div class="control">
+          <input
+            type="text"
+            placeholder="請輸入信箱"
+            v-model="registered.email"
+          />
+        </div>
+        <div class="control">
+          <input
+            type="text"
+            placeholder="請輸入密碼"
+            v-model="registered.password"
+          />
+        </div>
+        <div class="control">
+          <input class="submit" @click="handSubmit" value="Login" />
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Open sans";
+  color: #fff;
+}
+
+section {
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  background: url("~@/images/restaurant.jpg");
+  background-size: cover;
+  background-position: center center;
+}
+
+.form-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3));
+  width: 380px;
+  padding: 50px 30px;
+  border-radius: 10px;
+  box-shadow: 7px 7px 60px #000;
+}
+
+h1 {
+  text-transform: uppercase;
+  font-size: 2em;
+  text-align: center;
+  margin-bottom: 2em;
+}
+
+.control input {
+  width: 100%;
+  display: block;
+  padding: 10px;
+  color: #000;
+  border: none;
+  outline: none;
+  margin: 1em 0;
+  border-radius: 5px;
+}
+
+.submit {
+  background: crimson;
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 1.2em;
+  opacity: 0.7;
+  transform: opacity 0.3s ease;
+}
+
+.submit:hover {
+  opacity: 1;
+}
+</style>
