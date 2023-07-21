aa9fc074d5b5a0f42b4bb3c2e8834c10832dbbb5
judychang <judy8210276834@gmail.com>
2023-07-21 16:43:11 +0800

    card

--- src/components/home/Card.vue
+++ src/components/home/Card.vue
@@ -3,15 +3,26 @@
 import items from "../data/item.js";
 
 export default {
+    setup(){
 
+        return {
+            items
+        }
+    }
 }
 </script>
 
 <template>
-  
+  <div class="product-cards-container">
+
+  </div>
 </template>
 
 
-<style>
-
+<style scoped lang="scss">
+    .product-cards-container{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
 </style>
